26ab0c50a982323df610db2496bc7382710a6dca
Jesse Swedlund <swedlundjesse@gmail.com>
2021-02-09 16:35:35 -0800

    changed server for heroku deployment

--- server/index.js
+++ server/index.js
@@ -1,56 +1,24 @@
-const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
-const compression = require('compression')
+const path = require('path')
 const PORT = process.env.PORT || 8080
 const app = express()
+
+if (process.env.NODE_ENV !== 'testing') app.use(morgan('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/bundle.js'))
+})
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send({message: err.message})
+})
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
 module.exports = app
-
-const createApp = () => {
-  // logging middleware
-  app.use(morgan('dev'))
-
-  // body parsing middleware
-  app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
-
-  // compression middleware
-  app.use(compression())
-
-  // static file-serving middleware
-  app.use(express.static(path.join(__dirname, '..', 'public')))
-
-  // any remaining requests with an extension (.js, .css, etc.) send 404
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found')
-      err.status = 404
-      next(err)
-    } else {
-      next()
-    }
-  })
-
-  // sends index.html
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
-  })
-
-  // error handling endware
-  app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-  })
-}
-
-const startListening = () => {
-  app.listen(PORT, () => console.log(`Serving it up on ${PORT}`))
-}
-
-async function bootApp() {
-  await createApp()
-  await startListening()
-}
-
-bootApp()
