992caf2a979b14d5a8dee9c1463a45967419287b
Jesse Swedlund <swedlundjesse@gmail.com>
2024-03-03 14:26:27 -0800

    light updates

--- client/components/experienceData.js
+++ client/components/experienceData.js
@@ -1,79 +1,106 @@
 const experience = [
   {
+    title: 'Software Engineer II',
+    workplace: 'Carbon Direct',
+    location: 'Seattle, WA',
+    website: 'https://www.carbon-direct.com/',
+    dates: 'September 2022 - Present',
+    duties: [
+      'Building an end-to-end carbon management platform for companies to measure their carbon footprint, reduce their emissions, and buy high-quality carbon credits',
+      'Working across the stack: TypeScript, Next.js, React, Nest.js, Sequelize, AWS',
+      'Promoted from Software Engineer I within four months of hire',
+    ],
+    photo: 'images/CD-logo.jpg',
+  },
+  {
+    title: 'Consultant, Full stack Development',
+    workplace: 'Avanade Inc.',
+    location: 'Seattle, WA',
+    website: 'https://www.avanade.com/',
+    dates: 'May 2021 - August 2022',
+    duties: [
+      'Developed custom technical solutions for clients across the Microsoft ecosystem',
+      'Promoteded from Sr. Analyst within one year of hire',
+      'Obtained Azure-900 Certification',
+      'Volunteer social planner and intern instructor',
+    ],
+    photo: 'images/avanade-logo.jpg',
+  },
+  {
     title: 'Teaching Fellow',
     workplace: 'Fullstack Academy',
     location: 'Remote (New York, NY)',
     website: 'https://www.fullstackacademy.com/',
-    dates: 'January 2021 - Present',
+    dates: 'January 2021 - May 2021',
     duties: [
-      'Mentor, teach, and debug JavaScript with over 50 students',
-      'Assist student projects as a technical advisor and project manager',
-      'Perform weekly code reviews with students and lead weekly technical interview white-boarding problems',
-      'Interview prospective students for program admission'
+      'Mentored, taught, and debugged JavaScript with over 50 students',
+      'Assisted student projects as a technical advisor and project manager',
+      'Performed weekly code reviews with students and lead weekly technical interview white-boarding problems',
+      'Interviewed prospective students for program admission',
     ],
-    photo: 'images/fsa.png'
+    photo: 'images/fsa.png',
   },
   {
     title: 'Student',
     workplace: 'Fullstack Academy',
     location: 'Remote (New York, NY)',
     website: 'https://www.fullstackacademy.com/',
-    dates: 'August-December 2020',
+    dates: 'August - December 2020',
     major: 'Full-time Web Development Immersive Program Certificate',
     duties: [
       'Learned core technologies of React, Redux, Express, Sequelize, and PostgreSQL in 17-week top-rated program',
       'Developed three original full stack applications in final 6 weeks in a simulated project team work environment',
-      'Cultivated a growth mindset and ability to learn new technologies quickly and thoroughly'
+      'Cultivated a growth mindset and ability to learn new technologies quickly and thoroughly',
     ],
-    photo: 'images/fsa.png'
+    photo: 'images/fsa.png',
   },
   {
     title: 'Program Coordinator',
     workplace: 'Outdoors For All Foundation',
     location: 'Seattle, WA',
     website: 'https://outdoorsforall.org/',
-    dates: 'January 2019-July 2020',
+    dates: 'January 2019 - July 2020',
     duties: [
       'Managed skiing, climbing, kayaking, cycling, and camping programs for people with disabilities',
-      'Oversaw a 216% growth in participation and revenue in Saturday Night Ski Program'
+      'Oversaw a 216% growth in participation and revenue in Saturday Night Ski Program',
     ],
-    photo: 'images/ofa.jpg'
+    photo: 'images/ofa.jpg',
   },
   {
-    title: 'Lead Wilderness Instructor',
+    title: 'Wilderness Instructor',
     workplace: 'Voyageur Outward Bound School',
     location: 'Ely, MN; Redford, TX',
     website: 'https://www.vobs.org/',
-    dates: 'January 2014-July 2017',
+    dates: 'January 2014 - July 2017',
     duties: [
-      'Led up to 72-day wilderness expeditions,​ teaching lessons emphasizing leadership and communication',
-      'Created spaces of compassion, empathy, and support'
+      'Led up to 72-day wilderness expeditions, teaching lessons emphasizing leadership and communication',
+      'Created spaces of compassion, empathy, and support',
     ],
-    photo: 'images/vobs.jpg'
+    photo: 'images/vobs.jpg',
   },
   {
     title: 'Producer/Director',
     workplace: "Ty's List",
     location: 'Evanston, IL',
-    website: 'http://www.tyslist-themovie.com/',
-    dates: 'September 2011-December 2016',
+    website: 'http://www.tyslistthemovie.com/',
+    dates: 'September 2011 - December 2016',
     duties: [
-      'Co-Director/Producer of award-winning feature-length documentary film, “Ty’s List"'
+      `Co-Director/Producer of award-winning feature-length documentary film, “Ty's List"`,
     ],
-    photo: 'images/tyslist.jpg'
+    photo: 'images/tyslist.jpg',
   },
   {
     title: 'Student',
     workplace: 'Northwestern University',
     location: 'Evanston, IL',
     website: 'https://www.northwestern.edu/',
-    dates: 'September 2009-December 2013',
+    dates: 'September 2009 - December 2013',
     major: 'B.S. Radio,Television, and Film; Philosophy',
     duties: [
-      'Earned a double major Bachelor of Science degree from a top-ten university'
+      'Earned a double major Bachelor of Science degree from a top-ten university',
     ],
-    photo: 'images/NU.jpg'
-  }
+    photo: 'images/NU.jpg',
+  },
 ]
 
 export default experience
